dfd7bd79befb9b658ed6d0988acb376179ee6fe3
Cody Nettesheim <codynetty@gmail.com>
2024-02-07 18:23:26 -0700

    Add project files.

---
+++ Mission5Cody/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿$("#btnSubmit").click(function () {
+    var name = $("#txtName").val();
+    var hours = parseFloat($("#txtHours").val());
+
+    if (!isNaN(hours) && hours >= 0) {
+        var totalCost = hours * 25;
+        $("#totalOutput").text(name + "'s Total Cost: $" + totalCost.toFixed(2));
+
+        $("#txtName").val('');
+        $("#txtHours").val('');
+    } else {
+        alert("Please enter a valid positive numeric value for hours.");
+    }
+});
